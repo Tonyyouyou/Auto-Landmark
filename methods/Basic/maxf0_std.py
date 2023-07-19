3afba87714f95fa3c61b4ed8e6ab349c19b44dd4
Xiangyu Zhang <xzhan233@b17.clsp.jhu.edu>
2023-07-18 23:56:14 -0400

    maxf0

--- methods/Basic/maxf0_std.py
+++ methods/Basic/maxf0_std.py
@@ -1,58 +1,85 @@
 def maxf0_std(AGE_GENDER):
-    # Max's:
-    STDXM = 220
-    STDXF = 350
-    STDXC = 500
-    STDXI = 1200
-    STDXE = (STDXM + STDXF) / 2
+    # Flatten the input list
+    AGE_GENDER = [char for string in AGE_GENDER for char in string]
 
-    # Min's:
-    STDNM = STDXM / 4
-    STDNF = STDXF / 4
-    STDNC = STDXC / 5
-    STDNI = max(STDNC, STDXI / 8)
-    STDNE = min(STDNM, STDXE / 4)
+    # Function to process a single character
+    def maxf0_std_single_character(x):
+        if x == "?":
+            print("[maxf0, minf0] = maxf0_std(<'n'|AGE_GENDER|'m'|'f'|'i'|'c'|'e'>)")
+            return
 
-    if AGE_GENDER == '?':
-        print('[maxf0,<minf0>] = maxf0_std(<"n"|AGE_GENDER|"m"|"f"|"i"|"c"|"e">)')
-        return
+        if not x:
+            x = "N"  # Default = "N": adult, no gender-specific information
 
-    if AGE_GENDER is None:
-        AGE_GENDER = ''
+        # Max's:
+        STDXM = 220
+        STDXF = 350
+        STDXC = 500
+        STDXI = 1200
+        STDXE = (STDXM + STDXF) / 2
 
-    if len(AGE_GENDER) <= 1:
-        age_gender = AGE_GENDER.upper()
+        # Min's:
+        STDNM = STDXM / 4
+        STDNF = STDXF / 4
+        STDNC = STDXC / 5
 
-        if age_gender == 'M':
+        # 08/9/30: Was: STDNI = STDXI / 8; STDNE = STDXE / 4;
+        STDNI = max(STDNC, STDXI / 8)
+        STDNE = min(STDNM, STDXE / 4)
+
+        # Convert input character to upper case (case-insensitive)
+        x = x.upper()
+
+        if x == "M":
             maxf0 = STDXM
-        elif age_gender in ('F', 'N'):
-            maxf0 = STDXF
-        elif age_gender == 'C':
+        elif x in {"F", "N"}:
+            maxf0 = STDXF  # "N" => no info => use higher for max.
+        elif x == "C":
             maxf0 = STDXC
-        elif age_gender == 'I':
+        elif x == "I":
             maxf0 = STDXI
-        elif age_gender == 'E':
+        elif x == "E":
             maxf0 = STDXE
         else:
-            maxf0 = float('nan')
+            maxf0 = None
 
-        if age_gender in ('M', 'N'):
-            minf0 = STDNM
-        elif age_gender == 'F':
+        if x in {"M", "N"}:
+            minf0 = STDNM  # "N" => no info => use lowest for min.
+        elif x == "F":
             minf0 = STDNF
-        elif age_gender == 'C':
+        elif x == "C":
             minf0 = STDNC
-        elif age_gender == 'I':
+        elif x == "I":
             minf0 = STDNI
-        elif age_gender == 'E':
+        elif x == "E":
             minf0 = STDNE
         else:
-            minf0 = float('nan')
+            minf0 = None
 
-    else:
-        maxf01, minf01 = maxf0_std(AGE_GENDER[0])
-        maxf0, minf0 = maxf0_std(AGE_GENDER[1:])
-        maxf0 = [maxf01] + maxf0
-        minf0 = [minf01] + minf0
+        return maxf0, minf0
+
+    # Apply the function to each character in the list
+    record = list(map(maxf0_std_single_character,AGE_GENDER))
+
+    # Separate the maxf0 and minf0 values into separate lists
+    maxf0 = [item[0] for item in record]
+    minf0 = [item[1] for item in record]
 
     return maxf0, minf0
+
+# def test_maxf0_std():
+
+#     # Example 1
+#     x1 = maxf0_std('mf')
+#     print(x1)
+
+#     # Example 2
+#     x2 = maxf0_std(['m', 'f'])
+#     print(x2)
+
+#     # Example 3
+#     x3 = maxf0_std(['mfi', 'c', 'e'])
+#     print(x3)
+
+# # Run the test code
+# test_maxf0_std()
