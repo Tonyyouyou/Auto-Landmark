2ac4765cdfeb642dfef2a7afb412233dd1fe69dd
Xiangyu Zhang <tonyxiangyuzhang@gmail.com>
2023-10-06 15:15:57 +1100

    new

--- methods/Basic/Landmarks_func.py
+++ methods/Basic/Landmarks_func.py
@@ -1,8 +1,3 @@
-##############################################
-######### Author: Zhaocheng (David) Huang  #########
-##############################################
-# Modified by Xiangyu(Tony) Zhang
-
 import numpy as np
 import pandas as pd
 
